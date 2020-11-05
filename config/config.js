18bbfeb7afbccd649d663415cd8e0a4fd842fa72
Manikanta S <manikanta.s@triecoders.com>
2020-11-05 11:57:44 +0530

    add procfile

--- config/config.js
+++ config/config.js
@@ -22,7 +22,7 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    use_env_variable: "DATABASE_URL",
+    // use_env_variable: "DATABASE_URL",
     username: dbUsername,
     password: dbPassword,
     database: dbName,
